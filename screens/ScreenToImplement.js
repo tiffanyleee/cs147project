699752b2f7d7f825ecb823088d38d6687758955d
tiffanyleee <76767856+tiffanyleee@users.noreply.github.com>
2022-03-01 21:04:00 -0800

    formatted

--- screens/ScreenToImplement.js
+++ screens/ScreenToImplement.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 
 function rollDice() {
-  return Math.floor(Math.random() *6) + 1
+  return Math.floor(Math.random() * 6) + 1
 }
 
 export default function ScreenToImplement({ route, navigation }) {
-   const {message } = route.params;
+  const { message } = route.params;
 
   const [diceValue, setDiceValue] = useState("")
 
@@ -14,20 +14,20 @@ export default function ScreenToImplement({ route, navigation }) {
     <View style={styles.screenToImplement}>
       <Text style={styles.screenToImplement}>Screen To Implement</Text>
       <Button title="Go back to Home Screen" onPress={() => navigation.popToTop()} />
-      
+
       <Button title="Dice Roll" onPress={() => setDiceValue(rollDice())} />
       <Text>Dice Value: {diceValue}</Text>
       <Text>{message}</Text>
 
-      <GoBackButton title="Go Back To home screen"/>
+      <GoBackButton title="Go Back To home screen" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screenToImplement: {
-    flex: 1, 
-    alignItems: 'center', 
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
   },
   screenToImplementText: {
